c7862ed07ead555e82d5a5c73f755c7e4b24d045
NikitaCase <69657536+NikitaCase@users.noreply.github.com>
2021-01-18 22:25:09 -0500

    populate dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,26 @@
 // d3.json("data/samples.json").then(function (data) {console.log(data)})
 
-d3.json("samples.json").then((data) => { console.log(data)});
+// d3.json("samples.json").then((data) => { console.log(data)});
+
+// url = "https://github.com/NikitaCase/belly-biodiversity/blob/main/data/samples.json"
+
+// d3.json(url).then((data) => { console.log(data)});
+
+
+// nothing above works :( 
+
+
+
+var drop_down = d3.select("#select")
+
+var arr = ['a', 'b', 'c', 'd', 'e']
+
+for (var p = 0; p < arr.length; p++) {
+    var option = drop_down.append("option")
+    option.attr("value", arr[p])
+    option.text(arr[p])
+
+}
+
+
+/* <option value="t" >t</option> */
