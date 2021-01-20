4d53e34e86d4e939c3a9e7eb66316dc4327781f1
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-01-20 00:00:13 -0500

    hbar, style

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
 // Assigning variable names to tags which will be manipulated in html file 
 var drop_down = d3.select("#select")
 var ul_demog = d3.select("#demographics")
-
+var hbar = d3.select("#horizontal-bar")
+var guage = d3.select("#guage")
+var bubble = d3.select("#bubble")
 
 // Call the function which searches for the user ids 
 get_subject_ids()
@@ -27,10 +29,16 @@ function populate_drop_down(subject_ids) {
     }
 }
 
-// Display demographic data based on user selection 
-function display_subject_metadata() {
-
+function plot_data() {
     var user_subject = drop_down.node().value
+    display_subject_metadata(user_subject)
+    plot_hbar(user_subject)
+}
+
+// Display demographic data based on user selection 
+function display_subject_metadata(user_subject) {
+
+    // var user_subject = drop_down.node().value
 
     d3.json("data/samples.json").then((data) => {
 
@@ -40,7 +48,7 @@ function display_subject_metadata() {
         // Filter subjects by id (user input)
         var subject_metadata = all_subject_metadata.filter(row => row.id == user_subject)
 
-        console.log(subject_metadata)
+        // console.log(`metadata ${subject_metadata}`)
 
         // Clear previous output
         ul_demog.html("")
@@ -50,7 +58,7 @@ function display_subject_metadata() {
             Object.entries(pair).forEach(([key, value]) => {
                 var li = ul_demog.append("li")
                 li.text(`${key} : ${value}`)
-                console.log('here')
+                    // console.log('here')
             })
 
         });
@@ -60,5 +68,36 @@ function display_subject_metadata() {
 }
 
 
+function plot_hbar(user_subject) {
+
+    // var user_subject = drop_down.node().value
+
+    d3.json("data/samples.json").then((data) => {
+        var all_samples = data.samples
+
+        var subject_samples = all_samples.filter(row => row.id == user_subject)
+        var sample_values =
+
+            var otu_ids = subject_samples[0].otu_ids
+        var sorted_otu_ids = otu_ids.sort((a, b) => b - a)
+        var top_otu_ids = sorted_otu_ids.slice(0, 10)
+
+
+
+
+        hbar.html("")
+        console.log(otu_ids)
+
+
+        console.log(top_otu_ids)
+
+        // console.log(` samples ${subject_samples}`)
+
+    })
+
+}
+
+
+
 // Assign a listener to the drop down menu 
-drop_down.on("change", display_subject_metadata)
+drop_down.on("change", plot_data)
