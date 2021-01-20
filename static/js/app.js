6175a1c3811171e2d0fea57f155f3b920a76de40
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-01-19 23:04:21 -0500

    select, card <- json

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,64 @@
-// d3.json("data/samples.json").then(function (data) {console.log(data)})
-
-d3.json("data/samples.json").then((data) => { console.log(data) });
-
-
-// url = "https://github.com/NikitaCase/belly-biodiversity/blob/main/data/samples.json"
-
-// d3.json(url).then((data) => { console.log(data) });
-
-
-// nothing above works :( 
-
-
-
+// Assigning variable names to tags which will be manipulated in html file 
 var drop_down = d3.select("#select")
 var ul_demog = d3.select("#demographics")
 
-var arr = ['a', 'b', 'c', 'd', 'e']
 
-for (var p = 0; p < arr.length; p++) {
-    var option = drop_down.append("option")
-    option.attr("value", arr[p])
-    option.text(arr[p])
+// Call the function which searches for the user ids 
+get_subject_ids()
+
+function get_subject_ids() {
+    d3.json("data/samples.json").then((data) => {
+        // var subject_ids = data.names
+        var subject_ids = data.names.map(row => row)
+        populate_drop_down(subject_ids)
+
+    })
 
 }
+// Populate the drop down menu with the output from the previous function 
+function populate_drop_down(subject_ids) {
+    for (var p = 0; p < subject_ids.length; p++) {
 
 
-var dict = [{
-    a: 'aa',
-    b: 'bb',
-    c: 'cc',
-    d: 'dd'
-}]
+        var option = drop_down.append("option")
+        option.attr("value", subject_ids[p])
+        option.text(subject_ids[p])
 
-function print_subject_data() {
-    dict.forEach((pair) => {
+    }
+}
+
+// Display demographic data based on user selection 
+function display_subject_metadata() {
+
+    var user_subject = drop_down.node().value
+
+    d3.json("data/samples.json").then((data) => {
+
+        // Find demographic data on all subjects
+        var all_subject_metadata = data.metadata
+
+        // Filter subjects by id (user input)
+        var subject_metadata = all_subject_metadata.filter(row => row.id == user_subject)
+
+        console.log(subject_metadata)
+
+        // Clear previous output
+        ul_demog.html("")
 
 
-        Object.entries(pair).forEach(([key, value]) => {
-            var li = ul_demog.append("li")
-            li.text(`${key} : ${value}`)
-        })
+        subject_metadata.forEach(pair => {
+            Object.entries(pair).forEach(([key, value]) => {
+                var li = ul_demog.append("li")
+                li.text(`${key} : ${value}`)
+                console.log('here')
+            })
+
+        });
 
 
     })
 }
-print_subject_data()
 
 
-
-
-/* <option value="t" >t</option> */
+// Assign a listener to the drop down menu 
+drop_down.on("change", display_subject_metadata)
