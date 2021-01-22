a9f168b302c10f1258161bda150804b044760365
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-01-22 00:35:03 -0500

    guage, comments, style, ids
    index.html - page organisation ids, p tag areas for displaying instructions, comments
    style.css - center h5
    bonus.js - plot guage function
    app.js - comments added, functions renamed, changed text elements
    other - images added for readme

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,10 @@
 var drop_down = d3.select("#select")
 var ul_demog = d3.select("#demographics")
 var hbar = d3.select("#horizontal-bar")
-var guage = d3.select("#guage")
 var bubble = d3.select("#bubble")
+var bubble_title = d3.select("#bubble-title")
+var page_subtitle = d3.select("#page-subtitle")
+
 
 // Call the function which searches for the user ids 
 get_subject_ids()
@@ -17,6 +19,7 @@ function get_subject_ids() {
     })
 
 }
+
 // Populate the drop down menu with the output from the previous function 
 function populate_drop_down(subject_ids) {
     for (var p = 0; p < subject_ids.length; p++) {
@@ -26,96 +29,104 @@ function populate_drop_down(subject_ids) {
         option.attr("value", subject_ids[p])
         option.text(subject_ids[p])
 
+        bubble.text(" \n \n Select a test subject ID from the drop down menu to display the subject's data  \n \n ")
+
     }
 }
 
+
+// Gather user input and activate the plotting functions once listener is activated 
 function plot_data() {
     var user_subject = drop_down.node().value
     display_subject_metadata(user_subject)
-    plot_hbar(user_subject)
+    plot_sample_data(user_subject)
+
+    //  Clear first set of instructions and display second set 
+    bubble.text("")
+    page_subtitle.html("Hover over bars and circles on the graphs to reveal bacteria names")
+
 }
+
 
 // Display demographic data based on user selection 
 function display_subject_metadata(user_subject) {
 
-    // var user_subject = drop_down.node().value
-
+    // use d3 to load json file 
     d3.json("data/samples.json").then((data) => {
 
         // Find demographic data on all subjects
         var all_subject_metadata = data.metadata
 
-        // Filter subjects by id (user input)
+        // Filter metadata by id (user input)
         var subject_metadata = all_subject_metadata.filter(row => row.id == user_subject)
 
-        // console.log(`metadata ${subject_metadata}`)
+        // Isolate data that will be used to plot the guage from the bonus.js file 
+        var wfreq = subject_metadata[0].wfreq
+        plot_guage(wfreq)
 
-        // Clear previous output
+        // Clear previous output in div for demogrphics
         ul_demog.html("")
 
-
+        // Display demographics in a list 
         subject_metadata.forEach(pair => {
             Object.entries(pair).forEach(([key, value]) => {
                 var li = ul_demog.append("li")
                 li.text(`${key} : ${value}`)
-                    // console.log('here')
             })
-
         });
-
-
     })
 }
 
 
-function plot_hbar(user_subject) {
-
-    // var user_subject = drop_down.node().value
+// Plot data from sample data based on user selection 
+function plot_sample_data(user_subject) {
 
     d3.json("data/samples.json").then((data) => {
+
+        // Select all sample data  
         var all_samples = data.samples
 
+        // Filter by id (user input)
         var subject_samples = all_samples.filter(row => row.id == user_subject)
         var results = subject_samples[0]
         var { sample_values, otu_labels, otu_ids } = results
 
-
+        //  Plot the  bubble Chart
         var data_bubble = [{
             x: otu_ids,
             y: sample_values,
+            text: otu_labels,
             mode: 'markers',
             marker: {
-              size: sample_values, 
-              color: otu_ids
+                size: sample_values,
+                color: otu_ids
             }
-          }];
-          
-         var layout_bubble = {
-            title: 'bubble',
+        }];
+
+        var layout_bubble = {
             margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
-          }
-          };
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
 
-        var config = {responsive: true}
-        
-          
+        var config = { responsive: true }
+
         Plotly.newPlot("bubble", data_bubble, layout_bubble, config);
+        bubble_title.html(`Bacterial Groups Isolated from Subject ${user_subject}`)
 
-      
+
+        // Add Sample data to array to so data can be sorted, sliced, reversed  
         var samples_arr = []
 
-        for (var n=0; n<sample_values.length; n++) {
+        for (var n = 0; n < sample_values.length; n++) {
             var dict = {}
             dict.value = sample_values[n]
             dict.otu_id = otu_ids[n]
             dict.label = otu_labels[n]
-
             samples_arr.push(dict)
-
         }
 
         var sorted_results = samples_arr.sort((a, b) => b.value - a.value)
@@ -124,26 +135,20 @@ function plot_hbar(user_subject) {
 
         console.log(sorted_results)
 
-        var bar_data =[{
-            x: top_samples.map( a => a.value), 
-            y: top_samples.map(a => `otu_id: ${a.otu_id}`), 
-            text: top_samples.map(a => a.label), 
-            name: "Belly Flora", 
-            type: "bar", 
+
+        // Plot Bar Graph of top 10 samples 
+        var bar_data = [{
+            x: top_samples.map(a => a.value),
+            y: top_samples.map(a => `otu_id: ${a.otu_id}`),
+            text: top_samples.map(a => a.label),
+            name: "Belly Flora",
+            type: "bar",
             orientation: "h"
-           
+
         }]
 
-        var layout ={
-            title: "Belly Flora"
-        }
-
-        Plotly.newPlot("horizontal-bar", bar_data, layout)
-
-
-
+        Plotly.newPlot("horizontal-bar", bar_data, config)
     })
-
 }
 
 
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,1 +1,34 @@
 // guage 
+function plot_guage(wfreq) {
+    var data_guage = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wfreq,
+
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: {
+                range: [null, 9]
+            },
+            steps: [
+                { range: [0, 1], color: "#f8f3ec" },
+                { range: [1, 2], color: "#f4f1e5" },
+                { range: [2, 3], color: "#e9e6ca" },
+                { range: [3, 4], color: "#e5e7b3" },
+                { range: [4, 5], color: "#d5e49d" },
+                { range: [5, 6], color: "#b7cc92" },
+                { range: [6, 7], color: "#8cbf88" },
+                { range: [7, 8], color: "#8abb8f" },
+                { range: [8, 9], color: "#85b48a" }
+
+
+            ],
+
+        }
+    }];
+
+
+
+    // var layout_guage = { width: 100 % , height: 100 % , margin: { t: 0, b: 0 } };
+    Plotly.newPlot("guage", data_guage)
+}
